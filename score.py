2e5f7942c3639d9b7e9ec44acf5a860e954b0130
kieran <kieran.mace@gmail.com>
2017-02-24 10:42:58 -0800

    adding test cases as comments

--- score.py
+++ score.py
@@ -4,3 +4,9 @@ import numpy as np
 def get_score(guess, actual):
     diff = actual - guess
     return diff
+
+
+# get_score(1,1) should return 0
+# get_score(0,0) should return 0
+# get_score(1,0) should return Inf
+# get_score(0.5,1) should return 0.6931472
