b51c3375ca453014d6eb3b754e882c6c064e0457
lbogar <lucien.bogar@gmail.com>
2017-02-26 15:52:03 -0800

    adding basic logloss function

--- score.py
+++ score.py
@@ -1,10 +1,9 @@
 # This is the scoring function
 import numpy as np
 
-def get_score(guess, actual):
-    diff = actual - guess
-    return diff
-
+def get_logloss(guess, actual):
+    logloss = -np.mean(actual * np.log(guess)) + (1.0 - actual) * np.log((1.0 - guess))
+    return logloss
 
 # get_score(1,1) should return 0
 # get_score(0,0) should return 0
