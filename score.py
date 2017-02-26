46abf85c22cfcd92bbb4c9f9c7427c6008969734
Kieran Mace <kieran.mace@gmail.com>
2017-02-26 15:52:53 -0800

    added function to do all scores

--- score.py
+++ score.py
@@ -6,7 +6,15 @@ def get_score(guess, actual):
     return diff
 
 
-# get_score(1,1) should return 0
-# get_score(0,0) should return 0
-# get_score(1,0) should return Inf
-# get_score(0.5,1) should return 0.6931472
+# get_score(1.0,1.0) should return 0.0
+# get_score(0.0,0.0) should return 0.0
+# get_score(1.0,0.0) should return Inf
+# get_score(0.5,1.0) should return 0.6931472
+
+
+def score_predictions(truth, predicitons):
+    scores = list()
+    for i in range(0,len(truth)-1)
+        scores[i] = get_score[predicitons[i], truth[i]]
+    return np.mean(scores)
+    
