7a28657a06b55c6d42863583bdd2f5790e33a8d4
lbogar <lucien.bogar@gmail.com>
2017-02-26 16:43:20 -0800

    added apply_prediction, which applies a function to a pandas df

--- predict.py
+++ predict.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+
 def make_prediction(input):
     # Currently just going to return a random Number
     return np.random.random() # random number between 0 and 1
+
+def apply_prediction(dataframe):
+    dataframe['pred'] = dataframe['id'].apply(lambda x: make_prediction(x))
+    sample_submission.to_csv('test.csv',index=False)
+    return(dataframe)
