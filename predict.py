94006372e918a110e1b1aa1dd46835f6dbf3d541
Kieran Mace <kieran.mace@gmail.com>
2017-02-27 15:58:47 -0800

    added function to predict all inputs

--- predict.py
+++ predict.py
@@ -3,3 +3,8 @@ import numpy as np
 def make_prediction(input):
     # Currently just going to return a random Number
     return np.random.random() # random number between 0 and 1
+
+
+def predict_all(inputs):
+    predictions = [make_prediction(x) for x in inputs]
+    return predictions
