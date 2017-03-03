8ae11f0f334fdfda0e615473bf2b6a754cda3095
Kieran Mace <kieran.mace@gmail.com>
2017-03-03 15:55:51 -0800

    got rid of silly function that i wrote

--- predict.py
+++ predict.py
@@ -11,10 +11,6 @@ def make_prediction(input):
     #return np.random.random() # random number between 0 and 1
 
 
-def predict_all(inputs):
-    predictions = [make_prediction(x) for x in inputs]
-    return predictions
-
 def apply_prediction(dataframe):
     dataframe['pred'] = dataframe['id'].apply(lambda x: make_prediction(x))
     sample_submission.to_csv('test.csv',index=False)
